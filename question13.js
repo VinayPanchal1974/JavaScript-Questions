66e9707876665dc9edd871bea057d40604fcde12
VinayPanchal1974 <vinaypanchal115@gmail.com>
2024-12-20 20:25:30 +0530

    added 2 more ways to solve this question

--- question13.js
+++ question13.js
@@ -8,3 +8,23 @@ arr.forEach(elem =>{
     }
 })
 console.log(sum);
+
+// const arr = [3, "vinay", "nikhil", 1, "ramu", 2, "shamu", 4, "hwu", 5]
+// let sum1 = 0;
+// function sum(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (typeof arr[i] == 'number'){
+//             sum1 += arr[i]
+//         }
+//     }
+//     return sum1
+// }
+// console.log(sum(arr));
+
+// const arr = [3, "vinay", "nikhil", 1, "ramu", 2, "shamu", 4, "hwu", 5]
+// let sum1 = 0;
+// function sum(arr) {
+//     arr.forEach(e => (typeof e == 'number') ? sum1 += e : "")
+//     return sum1
+// }
+// console.log(sum(arr));
