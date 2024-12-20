65053ebb7ff08562a020de9dcec48d067788b599
VinayPanchal1974 <vinaypanchal115@gmail.com>
2024-12-20 12:33:42 +0530

    corrected a spelling mistake

--- question2.js
+++ question2.js
@@ -1,13 +1,6 @@
-// How to chek if and object is an array or not? provide some code
+// How to check if and object is an array or not? provide some code
 function isArray(obj) {
     return Array.isArray(obj)
 }
 console.log(isArray([]))
 console.log(isArray({}))
-
-// function arr(ele) {
-//     return Array.isArray(ele)
-// }
-
-// console.log(arr([]))
-// console.log(arr({}))
