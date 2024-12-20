66ba214af8aba4ed6bb2d533a2877a08cc6a7311
VinayPanchal1974 <vinaypanchal115@gmail.com>
2024-12-20 12:27:58 +0530

    added a new logic that can reverse string without predefinded function

--- question1.js
+++ question1.js
@@ -26,16 +26,30 @@
 // const answer = reverseString("my name is vinay")
 // console.log(answer);
 
-function reverseString(str) {
-    const arr = [];
-    for (let i = 0; i < str.length; i++) {
-        if(str[i]!== " "){
-            arr[i].push(arr)
-        }else if(str[i] === " "){
+// function reverseString(str) {
+//     const arr = [];
+//     for (let i = 0; i < str.length; i++) {
+//         if(str[i]!== " "){
+//             arr[i].push(arr)
+//         }else if(str[i] === " "){
 
-        }
-    }
-    return arr
-}
-const answer = reverseString("my name is vinay")
-console.log(answer);
+//         }
+//     }
+//     return arr
+// }
+// const answer = reverseString("my name is vinay")
+// console.log(answer);
+
+// const name1 = 'my name is vinay';
+// let finalName = '';
+// let changeName = '';
+// for (let i = 0; i < name1.length; i++) {
+//     if(name1[i]!== " "){
+//         changeName = name1[i] + changeName
+//     }else{
+//         finalName += changeName + " "
+//         changeName = ""
+//     }     
+// }
+// finalName = finalName + changeName;
+// console.log(finalName);
